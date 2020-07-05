1e17efc237e6dfd2203ebdce768d706de6910765
Abhishek Kanparia <kanpariaabhishek@gmail.com>
2020-07-05 19:50:29 +0530

    Add placeholder components to display errors

---
+++ src/components/Error/ErrorPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ErrorPage(props) {
+  return (
+    <div>
+      <h1>Unexpected error encountered, Please try again later.</h1>
+    </div>
+  )
+}
