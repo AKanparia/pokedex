b2efcdb2b1087c82d2e99168d4c741e8734d9ba9
Abhishek Kanparia <kanpariaabhishek@gmail.com>
2020-07-05 19:33:42 +0530

    Refactoring style

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,25 +1,20 @@
 import React from 'react'
-import PokeList from '../pokemon/PokeList'
 import styled from 'styled-components'
+import PokeList from '../pokemon/PokeList'
 
-const Container = styled.main`
+const DashBoardContainer = styled.main`
   padding-top: 80px;
-  display: flex;
-  flex-wrap: wrap;
-  align-items: space-between;
-  justify-content: center;
-  margin: 0 auto;
-  max-width: 1200px;
 
   @media (max-width: 768px) {
     padding-top: 55px;
   }
 `
+
 export default function DashBoard(props) {
   const { pokemons = [] } = props
   return (
-    <Container>
+    <DashBoardContainer>
       <PokeList pokemons={pokemons} />
-    </Container>
+    </DashBoardContainer>
   )
 }
--- src/components/pokemon/PokeList.js
+++ src/components/pokemon/PokeList.js
@@ -1,13 +1,23 @@
 import React, { Fragment } from 'react'
 import PokeCard from './PokeCard'
+import styled from 'styled-components'
+
+const PokeContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: stretch;
+  justify-content: center;
+  margin: 0 auto;
+  max-width: 1200px;
+`
 
 export default function PokeList(props) {
   const { pokemons = [] } = props
   return (
-    <Fragment>
+    <PokeContainer>
       {pokemons.map((pokemon) => (
         <PokeCard pokemon={pokemon} key={pokemon.id} />
       ))}
-    </Fragment>
+    </PokeContainer>
   )
 }
