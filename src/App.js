afad781b7108b43a825f229521e0075dcf16ea4d
Abhishek Kanparia <kanpariaabhishek@gmail.com>
2020-07-05 19:59:50 +0530

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
+import { Switch, Route } from 'react-router-dom'
 import NavBar from './components/layout/NavBar'
 import DashBoard from './components/layout/Dashboard'
-import styled from 'styled-components'
+import PageNotFound from './components/Error/PageNotFound'
+import ErrorPage from './components/Error/ErrorPage'
 
 const AppContainer = styled.div`
   margin: 0;
   height: 100%;
+`
+const Container = styled.main`
+  padding-top: 80px;
+
+  @media (max-width: 768px) {
+    padding-top: 55px;
+  }
 `
 
 function App() {
@@ -23,9 +33,22 @@ function App() {
     <AppContainer>
       <NavBar />
       {error ? (
-        <div className='error'>Error Encountered, Please try again later.</div>
+        <ErrorPage />
       ) : (
-        <DashBoard pokemons={pokemons} />
+        <Container>
+          <Switch>
+            <Route
+              path='/pokedex'
+              render={(props) => <DashBoard pokemons={pokemons} {...props} />}
+            />
+            <Route
+              path='/'
+              exact
+              render={(props) => <DashBoard pokemons={pokemons} {...props} />}
+            />
+            <Route component={PageNotFound} />
+          </Switch>
+        </Container>
       )}
     </AppContainer>
   )
--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,20 +1,8 @@
 import React from 'react'
-import styled from 'styled-components'
 import PokeList from '../pokemon/PokeList'
 
-const DashBoardContainer = styled.main`
-  padding-top: 80px;
-
-  @media (max-width: 768px) {
-    padding-top: 55px;
-  }
-`
-
+//TODO: Add filter options
 export default function DashBoard(props) {
   const { pokemons = [] } = props
-  return (
-    <DashBoardContainer>
-      <PokeList pokemons={pokemons} />
-    </DashBoardContainer>
-  )
+  return <PokeList pokemons={pokemons} />
 }
